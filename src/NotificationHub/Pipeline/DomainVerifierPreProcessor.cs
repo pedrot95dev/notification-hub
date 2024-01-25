bc968f2741a24bdc9c8f12eee3bfcc618d445a11
Pedro <pedrot95dev@gmail.com>
2024-01-25 00:56:58 +0000

    fix: DomainVerifierPreProcessor to work on development environment

--- src/NotificationHub/Pipeline/DomainVerifierPreProcessor.cs
+++ src/NotificationHub/Pipeline/DomainVerifierPreProcessor.cs
@@ -7,26 +7,48 @@ namespace NotificationHub.Pipeline;
 
 public class DomainVerifierPreProcessor : IGlobalPreProcessor
 {
-	public readonly IEnumerable<Application> _allowedApplications;
+	private readonly ILogger<DomainVerifierPreProcessor> _logger;
+	private readonly IWebHostEnvironment _environment;
+	private readonly IEnumerable<Application> _allowedApplications;
 
-	public DomainVerifierPreProcessor(IConfiguration configuration)
+	public DomainVerifierPreProcessor(
+		IConfiguration configuration,
+		ILogger<DomainVerifierPreProcessor> logger,
+		IWebHostEnvironment environment)
 	{
+		_logger = logger;
+		_environment = environment;
 		_allowedApplications = configuration.GetSection("Applications").Get<ApplicationsConfiguration>()?.Applications!;
 	}
 	
 	public Task PreProcessAsync(IPreProcessorContext ctx, CancellationToken ct)
 	{
+		if (_environment.IsDevelopment())
+		{
+			return Task.CompletedTask;
+		}
+		
 		if (!ctx.HttpContext.Request.Headers.TryGetValue("Host", out var host)
 			|| !ctx.HttpContext.Request.Headers.TryGetValue("Origin", out var origin))
 		{
 			return ctx.HttpContext.Response.SendUnauthorizedAsync(cancellation: ct);
 		}
 
-		if (_allowedApplications.All(x => x.Domain != new Uri(host).Host && x.Domain != new Uri(origin).Host))
+		try
 		{
-			return ctx.HttpContext.Response.SendUnauthorizedAsync(cancellation: ct);
+			var hostUri = new Uri(host);
+			var originUri = new Uri(origin);
+			
+			if (_allowedApplications.Any(x => x.Domain == hostUri.Host && x.Domain == originUri.Host))
+			{
+				return Task.CompletedTask;
+			}
+		}
+		catch (Exception ex)
+		{
+			_logger.LogWarning(ex, "Failed to parse host or origin header");
 		}
 		
-		return Task.CompletedTask;
+		return ctx.HttpContext.Response.SendUnauthorizedAsync(cancellation: ct);
 	}
 }
