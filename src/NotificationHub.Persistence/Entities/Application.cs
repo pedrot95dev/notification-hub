65c2c490c98b64fbb7503498b370a564e58a2054
Pedro <pedrot95dev@gmail.com>
2024-01-27 00:26:13 +0000

    chore: add external id to application entity

--- src/NotificationHub.Persistence/Entities/Application.cs
+++ src/NotificationHub.Persistence/Entities/Application.cs
@@ -5,21 +5,25 @@ namespace NotificationHub.Persistence.Entities;
 
 public class Application
 {
-	public Application(string domain, Guid smtpConfigurationId)
+	public Application(string externalId, string domain, Guid smtpConfigurationId)
 	{
 		Id = Guid.NewGuid();
+		ExternalId = externalId;
 		Domain = domain;
 		SmtpConfigurationId = smtpConfigurationId;
 	}
 	
-	public Application(string domain, SmtpConfiguration smtpConfiguration)
+	public Application(string externalId, string domain, SmtpConfiguration smtpConfiguration)
 	{
 		Id = Guid.NewGuid();
+		ExternalId = externalId;
 		Domain = domain;
 		SmtpConfiguration = smtpConfiguration;
 	}
 		
 	public Guid Id { get; set; }
+
+	public string ExternalId { get; set; }
 	
 	public Guid SmtpConfigurationId { get; set; }
 	
@@ -37,6 +41,8 @@ public class ApplicationConfiguration : IEntityTypeConfiguration<Application>
 	public void Configure(EntityTypeBuilder<Application> builder)
 	{
 		builder.HasKey(x => x.Id);
+		builder.Property(x => x.ExternalId)
+			.IsRequired();
 		builder.Property(x => x.Domain)
 			.IsRequired();
 		builder.Property(x => x.CreatedAt)
---
+++ src/NotificationHub.Persistence/Migrations/20240127002530_External_Id.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificationHub.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class External_Id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "Applications",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Applications");
+        }
+    }
+}
