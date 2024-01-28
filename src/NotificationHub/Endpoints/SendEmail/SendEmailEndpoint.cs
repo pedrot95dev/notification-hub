22673c4812ba9459c7a9c8a4cf58895defa8a3b9
Pedro <pedrot95dev@gmail.com>
2024-01-28 15:29:20 +0000

    feat: add email log on send email endpoint

--- src/NotificationHub/Endpoints/SendEmail/SendEmailEndpoint.cs
+++ src/NotificationHub/Endpoints/SendEmail/SendEmailEndpoint.cs
@@ -1,5 +1,8 @@
 ﻿using FastEndpoints;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using NotificationHub.Persistence;
+using NotificationHub.Persistence.Entities;
 using NotificationHub.Services.CurrentApplication;
 using NotificationHub.Services.EmailService;
 
@@ -28,13 +31,16 @@ public class SendEmailEndpoint : Endpoint<SendEmailRequest,
 {
 	private readonly ICurrentApplication _currentApplication;
 	private readonly IEmailService _emailService;
+	private readonly ApplicationDbContext _dbContext;
 
 	public SendEmailEndpoint(
 		ICurrentApplication currentApplication,
-		IEmailService emailService)
+		IEmailService emailService,
+		ApplicationDbContext dbContext)
 	{
 		_currentApplication = currentApplication;
 		_emailService = emailService;
+		_dbContext = dbContext;
 	}
 	
 	public override void Configure()
@@ -56,8 +62,23 @@ public class SendEmailEndpoint : Endpoint<SendEmailRequest,
 		var application = applicationResult.AsT0;
 		var smtpConfiguration = _currentApplication.SmtpConfiguration.AsT0;
 		
-		await _emailService.SendEmailAsync(req.Subject, req.Message, smtpConfiguration.UserName, application.EmailDestination, req.ReplayToEmail, smtpConfiguration, ct);
+		var emailsSentCount = await _dbContext.EmailSents
+			.Where(x => x.ApplicationId == application.Id)
+			.CountAsync(ct);
 		
+		var subject = $"#{emailsSentCount}: {req.Subject}";
+		
+		await _emailService.SendEmailAsync(subject, req.Message, smtpConfiguration.UserName, application.EmailDestination, req.ReplayToEmail, smtpConfiguration, ct);
+		
+		await LogEmailSent(application.Id, req.ReplayToEmail, ct);
+
 		await SendNoContentAsync(ct);
+	}
+
+	private async Task LogEmailSent(Guid applicationId, string email, CancellationToken ct)
+	{
+		var emailSent = new EmailSent(applicationId, email);
+		await _dbContext.EmailSents.AddAsync(emailSent, ct);
+		await _dbContext.SaveChangesAsync(ct);
 	}
 }
